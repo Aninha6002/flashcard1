0ecc94f728f9ac7425b215f38a28fc691b55c204
Aninha6002 <00001250945458sp@al.educacao.sp.gov.br>
2024-11-19 10:49:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lucas 9:54',
+    'Quais discípulos perguntaram a Jesus se podiam fazer descer fogo do céu ?',
+    'João e Tiago'
+)
+
+criaCartao(
+    'Filemom 1:2',
+    'A quem Paulo chamou de `meu companheiro de lutas`?',
+    'Arquipo'
+)
+
+criaCartao(
+    '2 Reis 18:4',
+    'Qual  era o nome da serpente de bronze que Moisés tinha feito?',
+    'Neustã'
+)
+
+criaCartao(
+    'Pergunta biblica',
+    'Em quais livros da Bíblia não encontramos a palavra `Deus`?',
+    'Ester e Cânticos'
+)
